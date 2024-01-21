155d5786ff4d62164f5a4c46a694c90a929b3eec
Alex.Mills <amills@vibeirl.com>
2024-01-21 01:33:23 -0500

    foa aoimt3

--- test/circular/main.go
+++ test/circular/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	jlog "github.com/oresoftware/json-logging/jlog"
-	"os"
+  jlog "github.com/oresoftware/json-logging/jlog/lib"
+  "os"
 )
 
-var log = jlog.New("vb", "vibe_", jlog.DEBUG, []*os.File{os.Stdout})
+var log = jlog.CreateLogger("vb").SetOutputFile(os.Stdout)
 
 func main() {
-	//jlog.DefaultLogger.Error("foo")
+  //jlog.DefaultLogger.Error("foo")
 
-	type M struct {
-		Foo string
-		Z   struct {
-			Bar string
-			Z   struct {
-				Bzz string
-				Z   interface {
-				}
-			}
-		}
-	}
+  type M struct {
+    Foo string
+    Z   struct {
+      Bar string
+      Z   struct {
+        Bzz string
+        Z   interface {
+        }
+      }
+    }
+  }
 
-	m := M{}
-	m.Z.Z.Z = &m
+  m := M{}
+  m.Z.Z.Z = &m
 
-	//log.Info(m)
-	log.Error(m)
+  //log.Info(m)
+  log.Error(m)
 }
